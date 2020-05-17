5ed7b7794576244a366f3f7332f48bd6fb31e8d0
Karol Adamiak <karol.kosciany@gmail.com>
2020-05-17 17:09:01 +0200

    small changes

--- ImagePuzzle/app/src/main/java/com/karol/imagepuzzle/MainActivity.kt
+++ ImagePuzzle/app/src/main/java/com/karol/imagepuzzle/MainActivity.kt
@@ -1,34 +1,21 @@
 package com.karol.imagepuzzle
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    private val imagesNames = arrayOf<String>(
-        "0BIVXIsbiAo.jpg",
-        "0vVkZ_BoRgE.jpg",
-        "7RTLxYt2lY4.jpg",
-        "BHa_5WrSC0g.jpg",
-        "cVZN7vSefq0.jpg",
-        "h6iGqwbC7Do.jpg",
-        "ICLBB9om6XU.jpg",
-        "KCcTQkS16i0.jpg",
-        "P6yYIRsvBCE.jpg",
-        "sCBKuXagfx4.jpg",
-        "XG_oD9-ZLKs.jpg",
-        "YXbgpClG7AY.jpg",
-        "z6A_uGtY51M.jpg",
-        "Z7trHpnA0O0.jpg"
-    )
+   private lateinit var pieces : ArrayList<Bitmap>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val assetManager = assets
-        asset
+       val layout = findViewById<ConstraintLayout>(R.id.puzzlePile)
 
 
 
